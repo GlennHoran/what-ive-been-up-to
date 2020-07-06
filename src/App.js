f880578b6adcf1ba640f93939e88f93e7ccc1e41
GLENN HORAN <GLENN.HORAN@LIBERTY-IT.CO.UK>
2020-07-06 14:48:44 +0100

    sticky footer

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import {Body} from "./components/Body"
 
 export default () => (
   <div className = "app">
+        {/*Have to have a seperate div for content to seperate it from footer to allow sticky footer to work : */}
+        {/*This was the most vanilla solution I could find and it's awesome compared to some others: https://dev.to/letsbsocial1/flexbox-sticky-footer-and-react-e1h*/}
+      <div className="content">
       <Header/>
-      <Body name = "Glenn"/>
+      <Body/>
+      </div>
       <Footer/>
   </div>
 );
