c3ac9d89a43d3983ef1004ce36c01affd147e758
GLENN HORAN <GLENN.HORAN@LIBERTY-IT.CO.UK>
2020-07-07 17:27:35 +0100

    minor changes

--- src/components/bodyComponents/TagButtonContainer.js
+++ src/components/bodyComponents/TagButtonContainer.js
@@ -4,13 +4,7 @@ import React from 'react'
 import '../../css/tagButtons.css'
 
 export default function TagButtonContainer() {
-    return <div className="tag-container">
-        <div>
-            Show me...
-        </div>
-        <div className="tag-button-container">
-            {tags.map(tag => {return <TagButton tag={tag.label}/>})}
-
-        </div>
+    return <div className="tag-button-container">
+        {tags.map(tag => {return <TagButton tag={tag.label}/>})}
     </div>
 }
